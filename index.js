ab713c23c3dd07a3aab25416bf604560ce9946fd
Arjith21 <arjith21@gmail.com>
2020-10-08 22:08:37 +0530

    modified changes from savin repo

--- index.js
+++ index.js
@@ -1,30 +1,66 @@
-const express = require("express");
 const cors = require('cors');
+const express = require('express');
+const stripe = require('stripe')('sk_test_51HO0KCESOl6tMOVpp2c81gag8m1uxOxbYBLzYxpLc6DWl4r2FwcVE5QDuWNYXar7MSqBj4dbG1Tfapj1FQIJ0lGp00m8KFS3ZQ');
 
 const { v4: uuidv4 } = require('uuid');
 const app = express();
+
+app.use(express.json());
 app.use(cors());
 
-const { resolve } = require("path");
-// This is your real test secret API key.
-const stripe = require("stripe")("sk_test_51HO0KCESOl6tMOVpp2c81gag8m1uxOxbYBLzYxpLc6DWl4r2FwcVE5QDuWNYXar7MSqBj4dbG1Tfapj1FQIJ0lGp00m8KFS3ZQ");
-app.use(express.static("."));
-app.use(express.json());
-const calculateOrderAmount = items => {
-  // Replace this constant with a calculation of the order's amount
-  // Calculate the order total on the server to prevent
-  // people from directly manipulating the amount on the client
-  return 1400;
-};
-app.post("/create-payment-intent", async (req, res) => {
-  const { items } = req.body;
-  // Create a PaymentIntent with the order amount and currency
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(items),
-    currency: "inr"
-  });
-  res.send({
-    clientSecret: paymentIntent.client_secret
-  });
+
+app.post('/retrieve-customer', async (req, res) => {
+  let customerData = {};
+  // res.send('Hello world');
+  const session = await stripe.checkout.sessions.retrieve(
+    req.body.sessionId, { stripeAccount: `${req.body.connectedAcc}` }
+  );
+  console.log('session----------' + JSON.stringify(session, null, 4));
+  let connectedAccCustomer
+  try {
+    connectedAccCustomer = await stripe.customers.retrieve(session.customer, { stripeAccount: `${req.body.connectedAcc}` });
+  }
+  catch ({ statusCode = 0 }) {
+    if (statusCode == 404) {
+      //------no customer exists
+    }
+  }
+  console.log('connectedAccCustomer----------' + JSON.stringify(connectedAccCustomer, null, 4));
+  const paymentIntent = await stripe.paymentIntents.retrieve(
+    session.payment_intent, { stripeAccount: `${req.body.connectedAcc}` }
+  );
+  console.log('paymentIntent----------' + JSON.stringify(paymentIntent, null, 4));
+  res.json(connectedAccCustomer);
 });
-app.listen(4242, () => console.log('Node server listening on port 4242!'));
+
+app.post('/create-checkout-session', async (req, res) => {
+  let connectedAccCustomer
+  try {
+    connectedAccCustomer = await stripe.customers.retrieve(req.body.customerId, { stripeAccount: `${req.body.connectedAcc}` });
+  }
+  catch ({ statusCode = 0 }) {
+    if (statusCode == 404) {
+      //------no customer exists
+    }
+  }
+
+  const session = await stripe.checkout.sessions.create({
+    payment_intent_data: {
+      setup_future_usage: 'off_session',
+    },
+    ...(connectedAccCustomer ? { customer: connectedAccCustomer.id } : {}),
+    payment_method_types: ['card'],
+    line_items: [{
+      price: `${req.body.priceId}`,
+      quantity: 1,
+    }],
+    mode: 'payment',
+    success_url: 'http://localhost:4200/ordersuccess?session_id={CHECKOUT_SESSION_ID}',
+    cancel_url: 'http://localhost:4200/',
+  }
+    , { stripeAccount: `${req.body.connectedAcc}` }
+  );
+  res.json({ id: session.id });
+});
+
+app.listen(3001, () => console.log('Listening on port 3001'));
